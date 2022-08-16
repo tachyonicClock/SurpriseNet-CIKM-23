4f9114592b18b1188bfd330b0c6a4775b4b90aba
LEE, ANTON JAMES (Anton) <anton.j.15.le@gmail.com>
2022-08-17 11:50:07 +1200

    Improve multi node experiment script

--- sync_remote.bash
+++ sync_remote.bash
@@ -2,7 +2,7 @@
 # Exit on first error
 set -e
 
-remotes="ml-23 ml-22"
+remotes="ml-23 ml-24"
 user_dir="/Scratch/al183"
 conda_dir="$user_dir/miniconda3"
 avalanche_dir="$user_dir/avalanche"
@@ -16,7 +16,7 @@ function push_remote {
     echo "--------------------------------------------------------------------------------"
     echo "Pushing to $1"
     echo "Setting up user directory"
-    ssh $1 "mkdir -p $user_dir"
+    ssh $1 "mkdir -p $user_dir $logs_dir"
 
     echo "Sync conda files"
     rsync -a --info=progress2 --delete $conda_dir $1:$user_dir
@@ -28,25 +28,32 @@ function push_remote {
     rsync -a --info=progress2 --delete $datasets_dir $1:$user_dir
 
     echo "Sync project files except logs"
-    rsync -a --info=progress2 --delete $project_dir --exclude=$logs_pattern $1:$user_dir
+    rsync -a --info=progress2 --delete $project_dir --exclude=$logs_pattern --exclude=nohup.out $1:$user_dir
 }
 
 function pull_results {
     echo "--------------------------------------------------------------------------------"
     echo "Pulling results from $1"
-    echo "Setting up user directory"
-
     echo "Pulling only log files"
-    rsync -a --info=progress2 $1:$logs_dir $logs_dir
+    rsync -a --info=progress2 $1:$logs_dir $project_dir
 }
 
 # Switch based on first argument
 case $1 in
     "pull")
+        for remote in $remotes; do
+            # Do not pull from self
+            if [ $remote != $(hostname) ]; then
+                pull_results $remote
+            fi
+        done
         ;;
     "push")
         for remote in $remotes; do
-            push_remote $remote
+            # Do not push to self
+            if [ $remote != $(hostname) ]; then
+                push_remote $remote
+            fi
         done
         ;;
     *)
